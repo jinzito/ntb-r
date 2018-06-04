3e1b01f76c3ee94bcd65db83110e76bc1b9dbd1d
Evgeny Rybakov <erybakov@exadel.com>
2018-06-04 17:24:38 +0300

    getting track details

--- src/scenes/tracks/TrackDetails.js
+++ src/scenes/tracks/TrackDetails.js
@@ -5,20 +5,56 @@ import PropTypes from 'prop-types';
 class TrackDetails extends Component {
 
 
-    //TODO get track info
+    constructor() {
+        super();
+        this.state = {hasData: false};
+    }
+
+    async componentDidMount() {
+
+        try {
+            const trackId = this.props.match.params._id;
+            const requestURL = "http://localhost:8080/api/track/" + trackId + "/";
+            const response = await fetch(requestURL, {
+                method: "POST"
+            });
+            const trackDetailsData = await response.json();
+            this.setState({track: trackDetailsData});
+        } catch (error) {
+            this.setState({track: null, error: "No track data"});
+            console.log(error);
+        } finally {
+            this.setState({hasData: true});
+        }
+    }
 
     render() {
+
+        if (!this.state.hasData) {
+            return(<p>Loading...</p>);
+        }
+
+        const track = this.state.track;
         return (
             <div>
-                <label>{"id:" + this.props.id + " title:" + this.props.title}</label>
+                <label>{"id:" + track._id + " 1 title:" + track.title}</label>
             </div>
         );
     }
 }
 
 TrackDetails.propTypes = {
-    _id: PropTypes.string.isRequired,
-    title: PropTypes.string
+    track: PropTypes.shape({
+        _id: PropTypes.string.isRequired,
+        initUrl: PropTypes.string,
+        storageId: PropTypes.string,
+        coverSrc: PropTypes.string,
+        title: PropTypes.string,
+        duration: PropTypes.number,
+        author: PropTypes.string,
+        errorStatus: PropTypes.string,
+        created: PropTypes.string
+    })
 };
 
 export default TrackDetails;
